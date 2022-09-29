58c506603de1a0f2dd66ef2acfb845d7410959ac
Valdemarcheck <valekzhaninvladimir58@gmail.com>
2022-09-29 19:13:54 +0500

    Setup the bubble sort algorithm

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 
 def bubble_sort(array)
-    array_copy = array
+    indexes = (0...array.length - 1)
+    loop do
+        sort_occured = false
+        for i in indexes do
+            if array[i] > array[i+1]
+                array[i], array[i+1] = array[i+1], array[i]
+                sort_occured = true
+            end
+        end
+        break if sort_occured == false
+    end
+    array
 end
 
-p bubble_sort([4,3,78,2,0,2]) == [0,2,2,3,4,78]
+p bubble_sort([4,3,78,2,0,2])
